9ea0cf60f2649e8cf542fd9783311733abf37112
DapperBlondie <alirezagharib110@gmail.com>
2021-08-21 19:53:14 +0430

    broadCastToAll, ListenToWsChannel, ListenForWS implemented

--- src/handlers/handlers.go
+++ src/handlers/handlers.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"fmt"
 	"github.com/DapperBlondie/simple-chat-app/src/render"
 	"github.com/gorilla/websocket"
 	zerolog "github.com/rs/zerolog/log"
@@ -31,6 +32,9 @@ type WsPayload struct {
 }
 
 var AppConf *ApplicationConfig
+
+var WsChan = make(chan *WsPayload)
+var Clients = make(map[*WSConnection]string)
 
 // TcpUpgrade use for upgrading HTTP request to TCP connection
 var TcpUpgrade = websocket.Upgrader{
@@ -73,10 +77,74 @@ func (ac *ApplicationConfig) WsEndpointHandler(w http.ResponseWriter, r *http.Re
 		MessageType: "Status",
 	}
 
+	conn := &WSConnection{MyConn: wsConn}
+	Clients[conn] = ""
+
 	err = wsConn.WriteJSON(resp)
 	if err != nil {
 		zerolog.Error().Msg(err.Error())
 		http.Error(w, "Error in sending the response to user over TCP connection", http.StatusInternalServerError)
 		return
+	}
+
+	go ListenForWS(conn)
+}
+
+// ListenForWS listening to every request and send them to
+func ListenForWS(conn *WSConnection) {
+	defer func() {
+		if r := recover(); r != nil {
+			zerolog.Error().Msg("error ; " + fmt.Sprintf("%v", r))
+		}
+	}()
+
+	payload := &WsPayload{
+		Action:   "",
+		Username: "",
+		Message:  "",
+		UserConn: nil,
+	}
+	for {
+		err := conn.MyConn.ReadJSON(&payload)
+		if err != nil {
+			err = conn.MyConn.WriteMessage(websocket.BinaryMessage, []byte(err.Error()))
+			if err != nil {
+				return
+			}
+		} else {
+			payload.UserConn = conn
+			WsChan <- payload
+		}
+	}
+}
+
+// ListenToWsChannel use for listening to our websocket channel and receiving *WsPayload
+func ListenToWsChannel() {
+	resp := &WsJsonResponse{
+		Action:      "",
+		Message:     "",
+		MessageType: "",
+	}
+	for {
+		e := <-WsChan
+
+		resp.Action = e.Action + "; Action"
+		resp.Message = fmt.Sprintf("Some message you sent : %v", e.Username)
+		broadCastToAll(resp)
+	}
+}
+
+// broadCastToAll use for broadCasting to all users
+func broadCastToAll(resp *WsJsonResponse) {
+	for client := range Clients {
+		err := client.MyConn.WriteJSON(resp)
+		if err != nil {
+			zerolog.Error().Msg(err.Error() + "; occurred in broadcasting")
+			err = client.MyConn.Close()
+			if err != nil {
+
+			}
+			delete(Clients, client)
+		}
 	}
 }
